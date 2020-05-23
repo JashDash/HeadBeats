b288c744196b4ac2e27be7b6c0d25a91fcea3737
Jashan Devgan <jashandevgan@gmail.com>
2020-05-23 12:28:39 +0530

    flac extension added

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,163 +1,211 @@
 <template>
-    <v-navigation-drawer app class="ExtendedProperties" expand-on-hover right height="27.2%">
+  <v-navigation-drawer
+    app
+    class="ExtendedProperties"
+    expand-on-hover
+    right
+    height="27.2%"
+  >
     <!--Change the height when adding new component-->
-        <v-list app class='pt-0 pb-0'>
-            <v-list-item-group>
-                <v-list-item class="Add-Music-Color" @click="PickSongs()">
-                    <v-list-item-icon class="Add-Music-Color">
-                        <v-icon class="white--text font-weight-bold">mdi-music-note-plus</v-icon>
-                    </v-list-item-icon >
-                    <v-list-item-title class="white--text font-weight-bold Add-Music-Color">Add Music</v-list-item-title>
-                </v-list-item>
-                <v-list-item class="New-Playlist-Color" @click.stop="PlaylistDialog = true">
-                    <v-list-item-icon class='New-Playlist-Color'>
-                        <v-icon class="white--text font-weight-bold">mdi-playlist-plus</v-icon>
-                    </v-list-item-icon >
-                    <v-list-item-title class="white--text font-weight-bold New-Playlist-Color" >New Playlist</v-list-item-title>
-                    <v-dialog v-model="PlaylistDialog" max-width="600">
-                        <v-card>
-                            <v-card-title class="headline">Name of the Playlist</v-card-title>
-                            <v-card-text>
-                                <v-text-field v-model="NewPlaylist" v-on:keyup.enter="PressedEnter()"></v-text-field>
-                            </v-card-text>
-                            <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn color="green darken-1" text @click="GetNewPlaylistName()"> Save </v-btn>
-                                <v-btn color="green darken-1" text @click="PlaylistDialog = false"> Close </v-btn>
-                            </v-card-actions>
-                        </v-card>
-                    </v-dialog>
-                </v-list-item>
-                <v-list-item class="Settings-Color">
-                    <v-list-item-icon class='Settings-Color'>
-                        <v-icon class="white--text font-weight-bold">mdi-cogs</v-icon>
-                    </v-list-item-icon >
-                    <v-list-item-title class="white--text font-weight-bold Settings-Color">Settings</v-list-item-title>
-                </v-list-item>
-                <v-list-item class="Help-Color">
-                    <v-list-item-icon class="Help-Color">
-                        <v-icon class="white--text font-weight-bold">mdi-help-circle</v-icon>
-                    </v-list-item-icon >
-                    <v-list-item-title class="white--text font-weight-bold Help-Color" >Help</v-list-item-title>
-                </v-list-item>
-                <!-- <v-divider></v-divider> -->
-            </v-list-item-group>
-        </v-list>
-    </v-navigation-drawer>
+    <v-list app class="pt-0 pb-0">
+      <v-list-item-group>
+        <v-list-item class="Add-Music-Color" @click="PickSongs()">
+          <v-list-item-icon class="Add-Music-Color">
+            <v-icon class="white--text font-weight-bold"
+              >mdi-music-note-plus</v-icon
+            >
+          </v-list-item-icon>
+          <v-list-item-title
+            class="white--text font-weight-bold Add-Music-Color"
+            >Add Music</v-list-item-title
+          >
+        </v-list-item>
+        <v-list-item
+          class="New-Playlist-Color"
+          @click.stop="PlaylistDialog = true"
+        >
+          <v-list-item-icon class="New-Playlist-Color">
+            <v-icon class="white--text font-weight-bold"
+              >mdi-playlist-plus</v-icon
+            >
+          </v-list-item-icon>
+          <v-list-item-title
+            class="white--text font-weight-bold New-Playlist-Color"
+            >New Playlist</v-list-item-title
+          >
+          <v-dialog v-model="PlaylistDialog" max-width="600">
+            <v-card>
+              <v-card-title class="headline">Name of the Playlist</v-card-title>
+              <v-card-text>
+                <v-text-field
+                  v-model="NewPlaylist"
+                  v-on:keyup.enter="PressedEnter()"
+                ></v-text-field>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="green darken-1"
+                  text
+                  @click="GetNewPlaylistName()"
+                >
+                  Save
+                </v-btn>
+                <v-btn
+                  color="green darken-1"
+                  text
+                  @click="PlaylistDialog = false"
+                >
+                  Close
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-list-item>
+        <v-list-item class="Settings-Color">
+          <v-list-item-icon class="Settings-Color">
+            <v-icon class="white--text font-weight-bold">mdi-cogs</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title class="white--text font-weight-bold Settings-Color"
+            >Settings</v-list-item-title
+          >
+        </v-list-item>
+        <v-list-item class="Help-Color">
+          <v-list-item-icon class="Help-Color">
+            <v-icon class="white--text font-weight-bold"
+              >mdi-help-circle</v-icon
+            >
+          </v-list-item-icon>
+          <v-list-item-title class="white--text font-weight-bold Help-Color"
+            >Help</v-list-item-title
+          >
+        </v-list-item>
+        <!-- <v-divider></v-divider> -->
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
-    import { remote } from 'electron';
-    import * as mm from 'music-metadata';
-    import * as util from 'util';
+import { remote } from "electron";
+import * as mm from "music-metadata";
+import * as util from "util";
 
-    function GetDisplayDuration(SongDuration) {
-        return (((SongDuration-(SongDuration%60))/60).toString(10) + 'min ' + (SongDuration%60).toString(10) + 'sec')
-    }
+function GetDisplayDuration(SongDuration) {
+  return (
+    ((SongDuration - (SongDuration % 60)) / 60).toString(10) +
+    "min " +
+    (SongDuration % 60).toString(10) +
+    "sec"
+  );
+}
 
-    export default {
-    
-    data: () => ({
-        PlaylistDialog: false,
-        AllNewSongs: [],
-        NewPlaylist: ""
-    }),
+export default {
+  data: () => ({
+    PlaylistDialog: false,
+    AllNewSongs: [],
+    NewPlaylist: "",
+  }),
 
-    methods: {
-        AddSongsToStore: function(GetNewSongs) {
-            const { dialog } = remote;
-            dialog.showOpenDialog({
-                properties: ['openFile', 'multiSelections'],
-                filters: [{
-                    name: 'Select Songs',
-                    extensions: ['mp3', 'wav', 'ogg']
-                }]
-            },async function(file) {
-                if(file) {
-                    var NewSongs = [];
-
-                    for(let i=0; i<file.length; ++i) {
-                        let Path = file[i];
-                         
-                        var NewSong = { 
-                            'Id': 0,
-                            'Source': Path,
-                            'Duration': 0,
-                            'DisplayDuration': 0,
-                            'Tags': [],
-                        }
-
-                        await mm.parseFile(NewSong.Source)
-                        .then( metadata => {
-                            util.inspect(metadata, {showHidden:true, depth: null});
-                            NewSong.Title = metadata.common.title
-                            NewSong.Duration = metadata.format.duration
-                            NewSong.DisplayDuration = GetDisplayDuration(Math.ceil(NewSong.Duration))
-                        });
-
-                        NewSongs.push(NewSong);
-                    }
-
-                    GetNewSongs(NewSongs);
-                }
-            })
+  methods: {
+    AddSongsToStore: function(GetNewSongs) {
+      const { dialog } = remote;
+      dialog.showOpenDialog(
+        {
+          properties: ["openFile", "multiSelections"],
+          filters: [
+            {
+              name: "Select Songs",
+              extensions: ["mp3", "wav", "ogg", "flac"],
+            },
+          ],
         },
+        async function(file) {
+          if (file) {
+            var NewSongs = [];
 
-        PickSongs: function() {
-            var AllNewSongs = this.$store;
+            for (let i = 0; i < file.length; ++i) {
+              let Path = file[i];
 
-            this.AddSongsToStore(function(AllSongs){
-                AllNewSongs.dispatch('AddNewSongs', AllSongs)
-            });
-        },
+              var NewSong = {
+                Id: 0,
+                Source: Path,
+                Duration: 0,
+                DisplayDuration: 0,
+                Tags: [],
+              };
 
-        PressedEnter: function() {
-            this.PlaylistDialog = false
-            this.NewPlaylist = ""; // Important to reintialize NewPlaylist Variable...
-        },
+              await mm.parseFile(NewSong.Source).then((metadata) => {
+                util.inspect(metadata, { showHidden: true, depth: null });
+                NewSong.Title = metadata.common.title;
+                NewSong.Duration = metadata.format.duration;
+                NewSong.DisplayDuration = GetDisplayDuration(
+                  Math.ceil(NewSong.Duration)
+                );
+              });
 
-        GetNewPlaylistName: function() {
-            this.PlaylistDialog = false;
-            // console.log(this.NewPlaylist);
-        },
+              NewSongs.push(NewSong);
+            }
 
-        CreateNewPlaylist: function() {
-            
-        },
-
-        OpenSettings: function() {
-            //
-        },
-
-        OpenHelp: function() {
-            //
+            GetNewSongs(NewSongs);
+          }
         }
-    }}
+      );
+    },
 
+    PickSongs: function() {
+      var AllNewSongs = this.$store;
+
+      this.AddSongsToStore(function(AllSongs) {
+        AllNewSongs.dispatch("AddNewSongs", AllSongs);
+      });
+    },
+
+    PressedEnter: function() {
+      this.PlaylistDialog = false;
+      this.NewPlaylist = ""; // Important to reintialize NewPlaylist Variable...
+    },
+
+    GetNewPlaylistName: function() {
+      this.PlaylistDialog = false;
+      // console.log(this.NewPlaylist);
+    },
+
+    CreateNewPlaylist: function() {},
+
+    OpenSettings: function() {
+      //
+    },
+
+    OpenHelp: function() {
+      //
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .ExtendedProperties {
-        margin-top: 300px; 
-        border-radius: 30px 0px 0px 30px;
-    }
-    .Add-Music-Color {
-        background-color: #7986CBFF; 
-    }
+.ExtendedProperties {
+  margin-top: 300px;
+  border-radius: 30px 0px 0px 30px;
+}
+.Add-Music-Color {
+  background-color: #7986cbff;
+}
 
-    .New-Playlist-Color {
-        background-color: #9575CDFF;
-    }
-    
-    .Settings-Color {
-        background-color: #BA68C8FF; 
-    }
-    
-    .Help-Color {
-        background-color: #F06292FF; 
-    }
-    
-    /* .AddColor {
+.New-Playlist-Color {
+  background-color: #9575cdff;
+}
+
+.Settings-Color {
+  background-color: #ba68c8ff;
+}
+
+.Help-Color {
+  background-color: #f06292ff;
+}
+
+/* .AddColor {
         background-color: #7986CBFF; 
     } */
 </style>
